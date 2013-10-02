0323c58703a21b77bb166e6fd9a343acb19a4813
Eric Nelson <Eric@kidtopro.com>
2013-10-02 12:57:28 -0500

    Added Login page

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>Peer Review</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/overrides.css" rel="stylesheet" media="screen">
+<!--   <link href="css/cosmo.css" rel="stylesheet" media="screen"> -->
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -12,12 +14,29 @@
       <script src="../../assets/js/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body>
-    <h1>HTDOCS!</h1>
-
+  <body style="text-align:center">
+  	<div class="clearfix danger">
+      	<h1>Peer Appraiser</h1>
+	</div>
+	<div>
+		<h2>Please Log In</h2>
+		<form action="verify.php" method="post">
+			<label for="username">User Name:</label><br>
+			<input type="text" placeholder="test" id="username" name="username"><br><br>
+			<label for="password">Password:</label><br>
+			<input type="password" id="password" name="password"><br><br>
+			<input type="submit" name="submit" value="Login">
+		</form>
+	</div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="//code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
+
+
+
+
+
+
